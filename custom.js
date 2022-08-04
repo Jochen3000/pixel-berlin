7fe5f142f622ada49e2dca0e7c49b02d9743843c
jochen ade <jochen.ade@springer.com>
2022-08-04 10:29:04 +0200

    first commit

---
+++ custom.js
@@ -0,0 +1,12 @@
+// ga4 custom click events
+const myLinkList = document.querySelectorAll('a');
+
+myLinkList.forEach(item => {
+    if (item.href.includes('webflow.')) {
+        item.addEventListener("click", () => {
+            gtag('event', 'webflow_click');
+        });
+    }
+});
+
+console.log('hallo');
